3029339c807984b2efcf48ac8599a0b14a698f75
weimeittx <422450455@qq.com>
2022-06-09 16:57:05 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,1 @@
+sudo ./cleos.sh --url http://localhost:8888 get info | grep fork_db_head_block_num; sudo ./cleos.sh --url https://wax.greymass.com get info | grep fork_db_head_block_num;sudo ./cleos.sh --url http://localhost:8888 get info | grep fork_db_head_block_num
