afcced5e0555a3514b7b51922d25c4dc7592c70e
422450455@qq.com <weimei865795615>
2022-12-13 22:50:52 +0800

    --

---
+++ test_local.sh
@@ -0,0 +1,3 @@
+cleos --url http://localhost:8888 get info | grep fork_db_head_block_num;
+cleos --url https://wax.greymass.com get info | grep fork_db_head_block_num;
+cleos --url http://localhost:8888 get info | grep fork_db_head_block_num;
